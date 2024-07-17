64e77bb1af9fc59b97af6622b4aebd2c1c10f603
Tushar Sahni <65815842+tusharsahni22@users.noreply.github.com>
2024-07-17 12:32:59 +0530

    changes api address

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://fomofactory.onrender.com/api",
+    baseURL: "https://www.backendfomo.streetswear.in/api",
     headers: {
         "Content-Type": "application/json",
     },
